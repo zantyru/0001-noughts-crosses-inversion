8bc412c65cd7d2e8803c77108a913da792acee48
Евгений Заонегин <zaonegin_s@mail.ru>
2019-02-15 19:45:23 +0300

    Remove ability of saving settings

--- scenes2/_global/Global.gd
+++ scenes2/_global/Global.gd
@@ -5,60 +5,12 @@ const CONST = preload("res://CONST.gd")
 const SAVE_PATH = "user://tic-tac-inverse.sav"
 
 
-var difficulty
-var first_turn
-var o_is_human
-var x_is_human
-var o_inversions
-var x_inversions
-
-
-static func new_settings():
-    var settings = {
-        "difficulty": CONST.DIFFICULTY.LOW,
-        "first_turn": CONST.SIGNUM.CIRCLE,
-        "o_is_human": true,
-        "x_is_human": false,
-        "o_inversions": 1,
-        "x_inversions": 1,
-    }
-    return settings
-
-
-static func save_settings(settings):
-    print("%s: saving settings" % OS.get_ticks_msec())
-    var file = File.new()
-    file.open(SAVE_PATH, File.WRITE)
-    file.store_line(to_json(settings))
-    file.close()
-
-
-static func load_settings():
-    var settings
-    print("%s: loading settings" % OS.get_ticks_msec())
-    var file = File.new()
-    # Make sure a saved settings actually exists
-    var settings_found = file.file_exists(SAVE_PATH)
-    # If it does, open the file, read/parse its contents, and return them
-    if settings_found:
-        print("%s: existing save found; loading" % OS.get_ticks_msec())
-        file.open(SAVE_PATH, File.READ)
-        settings = parse_json(file.get_as_text())
-    # If no saved settings exists, make a new one
-    else:
-        print("%s: no existing save found; making fresh settings" % OS.get_ticks_msec())
-        settings = new_settings()
-    return settings
-
-
-func _ready():
-    var settings = load_settings()
-    difficulty = settings.difficulty
-    first_turn = settings.first_turn
-    o_is_human = settings.o_is_human
-    x_is_human = settings.x_is_human
-    o_inversions = settings.o_inversions
-    x_inversions = settings.x_inversions
+var difficulty = CONST.DIFFICULTY.LOW
+var first_turn = CONST.SIGNUM.CIRCLE
+var o_is_human = true
+var x_is_human = false
+var o_inversions = 1
+var x_inversions = 1
 
 
 func update_difficulty():
@@ -76,15 +28,3 @@ func update_difficulty():
         Global.o_inversions = new_value
     if Global.x_is_human:
         Global.x_inversions = new_value
-
-
-func get_settings_as_dictionary():
-    var settings = {
-        "difficulty": difficulty,
-        "first_turn": first_turn,
-        "o_is_human": o_is_human,
-        "x_is_human": x_is_human,
-        "o_inversions": o_inversions,
-        "x_inversions": x_inversions,
-    }
-    return settings
